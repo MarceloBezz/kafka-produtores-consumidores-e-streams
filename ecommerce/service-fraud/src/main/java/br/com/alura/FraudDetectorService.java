d38ca2fee1626a0cdfece6f7af046d6236995ac1
Marcelo <mdbezerra16@gmail.com>
2025-11-14 22:50:55 -0300

    feat: :sparkles: Divisão do projeto em submódulos (microsserviços)

--- ecommerce/service-fraud/src/main/java/br/com/alura/FraudDetectorService.java
+++ ecommerce/service-fraud/src/main/java/br/com/alura/FraudDetectorService.java
@@ -7,7 +7,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
+        var service = new KafkaService<Order>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
                 fraudService::parse,
                 Order.class,
                 Map.of());
